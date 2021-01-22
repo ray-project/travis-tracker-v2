c0ccdcebdd757677503997ae590326bd4a4cc28c
simon-mo <simon.mo@hey.com>
2021-01-21 17:08:52 -0800

    update criteria

--- slack_fail_runner.py
+++ slack_fail_runner.py
@@ -14,7 +14,7 @@ SELECT test_name, COUNT(*) as failed_count
 FROM test_result, commits
 WHERE test_result.sha == commits.sha
   AND status == 'FAILED'
-  AND commits.idx < 4
+  AND commits.idx <= 5
 GROUP BY test_name
   HAVING COUNT(*) >= 3
 """
@@ -26,8 +26,7 @@ if len(failed_tests) == 0:
 
 markdown_lines = ["🚧 Your Failing Test Report"]
 for name, count in failed_tests:
-    markdown_lines.append(f"- `{name}` failed *{count}* times over latest 4 "
-                          f"tests.")
+    markdown_lines.append(f"- `{name}` failed *{count}* times over latest 5 commits")
 markdown_lines.append("Go to https://flakey-tests.ray.io/ to view Travis links")
 slack_url = os.environ["SLACK_WEBHOOK"]
 slack_channnel = os.environ.get("SLACK_CHANNEL_OVERRIDE", "#open-source")
