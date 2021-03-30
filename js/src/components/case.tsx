fda10f9f2011195b592102f3eafb19fbcf8d54c7
simon-mo <simon.mo@hey.com>
2021-03-30 11:56:14 -0700

    Add fail rate

--- js/src/components/case.tsx
+++ js/src/components/case.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import { Col, Row, Typography } from "antd";
 import { BugFilled } from "@ant-design/icons";
-import { SiteFailedTest } from "../interface";
+import { SiteCommitTooltip, SiteFailedTest } from "../interface";
 import SegmentedBar from "./segment";
 import DetailModal from "./detail";
 
 interface Prop {
   case: SiteFailedTest;
 }
+
+const getRate = (
+  testCase: SiteFailedTest,
+  mapFunc: (SiteCommitTooltip) => number
+) => {
+  const totalFailed: number = testCase.status_segment_bar
+    .map(mapFunc)
+    .reduce((a, b) => a + b);
+  return totalFailed;
+};
 
 const TestCase: React.FC<Prop> = (props) => {
   const [showModal, setShowModal] = useState(false);
@@ -32,14 +42,25 @@ const TestCase: React.FC<Prop> = (props) => {
         <Row>
           <Col>{props.case.name}</Col>
           <Col flex="auto"></Col>
+          <Col span={3}>
+            <div className="item failed" style={{ marginTop: "3px" }}></div>
+            {getRate(props.case, (t) => t.num_failed || 0).toFixed(0)}%
+          </Col>
+          <Col span={3}>
+            <div className="item failed" style={{ marginTop: "3px" }}></div>
+            <div className="item flaky" style={{ marginTop: "3px" }}></div>
+            {getRate(
+              props.case,
+              (t) => (t.num_flaky || 0) + (t.num_failed || 0)
+            ).toFixed(0)}
+            %
+          </Col>
           <Col>
             <BugFilled />
           </Col>
         </Row>
       </button>
-      <SegmentedBar
-        commits={props.case.status_segment_bar}
-      ></SegmentedBar>
+      <SegmentedBar commits={props.case.status_segment_bar}></SegmentedBar>
       <DetailModal
         testName={props.case.name}
         visible={showModal}
