748ecdf92133fa2adc6151b1a232c0bcd213d8e2
simon-mo <simon.mo@hey.com>
2021-09-07 10:18:57 -0700

    fix sql

--- sql/slow_tests.sql
+++ sql/slow_tests.sql
@@ -1,9 +1,11 @@
 -- SQLite
-SELECT test_name, (SUM(test_duration_s)/COUNT(*)) as mean_duration_s
+.mode table
+
+SELECT test_name, owner, (SUM(test_duration_s)/COUNT(*)) as mean_duration_s
 FROM test_result,commits
-WHERE test_name NOT LIKE "%travis%"
-  AND test_name NOT LIKE "bk:%"
+WHERE owner NOT LIKE "%infra%"
 AND test_result.sha == commits.sha
 AND commits.idx <= 20
 GROUP BY test_name
 ORDER BY mean_duration_s DESC
+LIMIT 10
