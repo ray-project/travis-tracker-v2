73be8ff9c9e4e0a8dbfb99f2fc13a6f563646f30
Jun Gong <jungong@anyscale.com>
2022-04-20 14:01:07 -0700

    fix

--- slack_flaky_runner.py
+++ slack_flaky_runner.py
@@ -12,7 +12,7 @@ top_failed_tests = list(
         """
 SELECT
   test_name,
-  MIN(commits.idx) as most_recent_failure
+  MIN(commits.idx) as most_recent_failure,
   COUNT(*) as failed_count
 FROM test_result, commits
 WHERE test_result.sha == commits.sha
@@ -24,17 +24,21 @@ ORDER BY failed_count DESC;
 """
     )
 )
-if len(top_failed_tests) == 0:
-    print("No failed cases, skipping.")
-    sys.exit(0)
 
+has_flaky_tests = False
 markdown_lines = ["🚓 Your Flaky Test Report of the Day (posted 9AM each weekday)"]
 for name, most_recent, count in top_failed_tests:
     if most_recent > 2:
         # Most recent 3 runs have been successful. Test may have been fixed.
         continue
+    has_flaky_tests = True
     markdown_lines.append(f"- `{name}` failed *{count}* times over latest 20 tests.")
 markdown_lines.append("Go to https://flakey-tests.ray.io/ to view Travis links")
+
+if not has_flaky_tests:
+    print("No failed cases, skipping.")
+    sys.exit(0)
+
 slack_url = os.environ["SLACK_WEBHOOK"]
 slack_channnel = os.environ.get("SLACK_CHANNEL_OVERRIDE", "#open-source")
 
